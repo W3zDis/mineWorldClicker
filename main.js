ea43a76216412e7e2a872ef67ab5894f3ac2ed61
luistonicelli@gmail.com <luistonicelli@gmail.com>
2022-07-16 01:45:06 -0300

    Primeira atualização, adicionei multiplicadores e a base do jogo

---
+++ main.js
@@ -0,0 +1,39 @@
+let multiplicador = 1
+let canvas = document.getElementById('canv')
+let ctx = canvas.getContext('2d')
+
+guardarClick = document.getElementById('clickEvent')
+clicks = 0
+moeda = 0
+maxClicks = 10
+
+guardarClick.addEventListener('click', evt => {
+  clickSet()
+  document.getElementById('numberScore').innerText = moeda
+  console.log(clicks)
+  
+})
+
+function clickSet() {
+  clicks += 1
+  if (clicks >= maxClicks) {
+    maxClicks += 10
+    moeda ++ 
+    //multiplicadores
+  } else if (multiplicador == 1) {
+    moeda += 10
+  } else if (multiplicador == 2) {
+    moeda += 53
+  } else if (multiplicador == 3) {
+    moeda += 100
+  }
+}
+
+/* function multi1() {
+  
+  }
+} */
+
+//canvas
+
+
